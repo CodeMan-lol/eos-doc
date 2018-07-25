9277b21c0bfbd5d7d16d72310bc9e0488d6ab48f
syavingc <syaving@outlook.com>
2018-07-25 23:18:51 +0800

    update ubuntu_init

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -2,7 +2,7 @@
 
 #update os & install software
 apt-get update -y
-apt install -y lrzsz tree htop python-pip git wget telnet vim
+apt install -y lrzsz tree htop python-pip git wget telnet vim unzip 
 
 #useradd eosio
 useradd -m -s /bin/bash eosio
@@ -10,9 +10,9 @@ echo "eosio:111111" | chpasswd
 
 #
 mkdir -p /data/eos-config
+mkdir -p /data/eos-data
 touch /data/eos-config/genesis.json
 touch /data/eos-config/config.ini
-mkdir -p /data/eos-data
 touch /data/eosio.log
 touch /data/eoskeeper.log
 
@@ -43,3 +43,8 @@ eof
 #
 hostname bakserver
 echo bakserver > /etc/hostname
+
+
+#clone eosio
+mkdir -p /opt/EOS-Mainnet && cd /opt/EOS-Mainnet
+git clone https://github.com/EOS-Mainnet/eos.git --recursive
